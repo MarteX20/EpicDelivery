a770f7ef8f364e03742dc5318bf77e8f664b6027
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-09-09 12:14:53 +0200

    Initial commit

---
+++ src/main/java/capstone/EpicDelivery/EpicDeliveryApplication.java
@@ -0,0 +1,13 @@
+package capstone.EpicDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/EpicDelivery/EpicDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.EpicDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
