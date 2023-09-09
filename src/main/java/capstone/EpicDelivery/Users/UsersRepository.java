b34cb1fd0d7aa82bc1dfeb4aaada29b463ef7478
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-09-09 12:31:15 +0200

    Creazione Repository di User

--- src/main/java/capstone/EpicDelivery/Users/UsersRepository.java
+++ src/main/java/capstone/EpicDelivery/Users/UsersRepository.java
@@ -1,4 +1,12 @@
 package capstone.EpicDelivery.Users;
 
-public class UsersRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
